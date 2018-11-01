e850495e4fbc4805c4a41a048be9379ed3f683ef
RAMYA <sivramya12@gmail.com>
2018-11-01 16:57:11 -0400

    Leave time first test

--- BabySitterKata.Tests/BabySitterPayCalculatorTestfiles/BabySitterPayCalculatorTest.cs
+++ BabySitterKata.Tests/BabySitterPayCalculatorTestfiles/BabySitterPayCalculatorTest.cs
@@ -6,35 +6,39 @@ namespace BabySitterKata.Tests.BabySitterPayCalculatorTestfiles
     [TestClass]
     public class BabySitterPayCalculatorTest
     {
+        BabySitterPayCalculator b = new BabySitterPayCalculator();
         [TestMethod]
         public void startsNoEarlierThan5PM()
         {
-            string isStartTimeValid;
-            BabySitterPayCalculator b = new BabySitterPayCalculator();
-            //checking starting time exactly 5PM
-            isStartTimeValid = b.startTimeValidate(5,"PM");
-            Assert.AreEqual("valid", isStartTimeValid);
+            //checking starting time exactly 5PM         
+            Assert.AreEqual("valid", b.startTimeValidate(5, "PM"));
 
             //checking starting time after 5PM
-            isStartTimeValid = b.startTimeValidate(6, "PM");
-            Assert.AreEqual("valid", isStartTimeValid);
+            Assert.AreEqual("valid", b.startTimeValidate(6, "PM"));
 
-            //checking starting time before 5PM
-            isStartTimeValid = b.startTimeValidate(4, "PM");
-            Assert.AreEqual("invalid", isStartTimeValid);
+            //checking starting time before 5PM           
+            Assert.AreEqual("invalid", b.startTimeValidate(4, "PM"));
 
             //checking for staring time as 12AM
-            isStartTimeValid = b.startTimeValidate(12, "AM");
-            Assert.AreEqual("valid", isStartTimeValid);
+            Assert.AreEqual("valid",b.startTimeValidate(12, "AM"));
 
             //checking for staring time as 1AM to ensure it accepts valid AM values as well
-            isStartTimeValid = b.startTimeValidate(1, "AM");
-            Assert.AreEqual("valid", isStartTimeValid);
+            Assert.AreEqual("valid", b.startTimeValidate(1, "AM"));
 
-            //checking for staring time as 4AM to ensure it won't accepts invalid AM values
-            isStartTimeValid = b.startTimeValidate(4, "AM");
-            Assert.AreEqual("invalid", isStartTimeValid);
+            //checking for staring time as 4AM to ensure it won't accepts invalid AM values         
+            Assert.AreEqual("invalid", b.startTimeValidate(4, "AM"));
 
         }
-    }
+
+        [TestMethod]
+        public void leavesNoLaterThan4AM()
+        {
+            //checking for leaving time 3 AM
+            Assert.AreEqual("valid", b.endTimeValidate(3, "AM"));
+
+        }
+
+
+
+        }
 }
--- BabySitterKata/Models/BabySitterPayCalculator.cs
+++ BabySitterKata/Models/BabySitterPayCalculator.cs
@@ -10,22 +10,27 @@ namespace BabySitterKata.Models
         public string startTimeValidate(float starttime,string amorpm)
         {
            //start time no later than 5PM
-            if((starttime>=5.0)&&(amorpm=="PM"))
-            
-                return ("valid");
-            
+            if((starttime>=5.0)&&(amorpm=="PM"))           
+            return ("valid");
             else
             {
                 //AM validation
                 if ((starttime == 12) && (amorpm == "AM"))
                     return ("valid");
-                else if ((starttime >= 1.0) && (starttime < 4.0) && (amorpm == "AM"))
-                
-                    return ("valid");
-                
+                else if ((starttime >= 1.0) && (starttime < 4.0) && (amorpm == "AM"))                
+                    return ("valid");               
                 else
                     return ("invalid");
             }
         }
+
+        public string endTimeValidate(float endTime, string amorpm)
+        {
+            if ((endTime <= 4) && (amorpm == "AM"))
+                return ("valid");
+            else
+                return ("invalid");
+        }
+
     }
 }
