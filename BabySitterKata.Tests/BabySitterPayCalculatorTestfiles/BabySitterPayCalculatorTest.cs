db22b8f880c3b26688129e9aa0c093faf8ca4a06
RAMYA <sivramya12@gmail.com>
2018-11-01 11:24:08 -0400

    FirstTest

---
+++ BabySitterKata.Tests/BabySitterPayCalculatorTestfiles/BabySitterPayCalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabySitterKata.Models;
+namespace BabySitterKata.Tests.BabySitterPayCalculatorTestfiles
+{
+    [TestClass]
+    public class BabySitterPayCalculatorTest
+    {
+        [TestMethod]
+        public void startsNoEarlierThan5PM()
+        {
+            BabySitterPayCalculator b = new BabySitterPayCalculator();
+            //checking starting time exactly 5PM
+            string isStarttimeValid = b.startTimeValidate(5,"PM");
+            Assert.AreEqual("valid", isStarttimeValid);
+        }
+    }
+}
---
+++ BabySitterKata/Models/BabySitterPayCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BabySitterKata.Models
+{
+    public class BabySitterPayCalculator
+    {
+        public string startTimeValidate(float time,string amorpm)
+        {
+           
+            if((time>=5.0)&&(amorpm=="PM"))
+            {
+                return ("valid");
+            }
+            else
+            {
+                return ("invalid");
+            }
+        }
+    }
+}
